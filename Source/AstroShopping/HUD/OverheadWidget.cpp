81d83a8ac07bd725ee90e44698eaf02d0fff5ec9
Tim <herew26@gmail.com>
2025-02-18 22:24:03 +0300

    ✨ Add OverheadWidget for displaying player network roles in HUD

---
+++ Source/AstroShopping/HUD/OverheadWidget.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "OverheadWidget.h"
+#include "Components/TextBlock.h"
+
+void UOverheadWidget::SetDisplayText(FString Text)
+{
+	if (!DisplayText) return;
+
+	DisplayText->SetText(FText::FromString(Text));
+}
+
+void UOverheadWidget::ShowPlayerNetRole(APawn* InPawn)
+{
+	ENetRole RemoteRole = InPawn->GetRemoteRole();
+	FString Role;
+
+	switch (RemoteRole)
+	{
+	case ENetRole::ROLE_Authority:
+		Role = "Authority";
+		break;
+	case ENetRole::ROLE_AutonomousProxy:
+		Role = "AutonomousProxy";
+		break;
+	case ENetRole::ROLE_SimulatedProxy:
+		Role = "SimulatedProxy";
+		break;
+	case ENetRole::ROLE_None:
+		Role = "None";
+		break;
+	}
+	
+	FString RemoteRoleString = FString::Printf(TEXT("Remote Role: %s"), *Role);
+	SetDisplayText(RemoteRoleString);
+}
+
+void UOverheadWidget::NativeDestruct()
+{
+	RemoveFromParent();
+	Super::NativeDestruct();
+}
