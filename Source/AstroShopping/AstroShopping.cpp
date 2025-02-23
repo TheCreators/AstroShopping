2c81bbedaf627c6ff96584b2bf1d07c964f820f0
Tim <herew26@gmail.com>
2025-02-23 20:00:02 +0300

    💡 Remove copyright notices from source files

--- Source/AstroShopping/AstroShopping.cpp
+++ Source/AstroShopping/AstroShopping.cpp
@@ -1,5 +1,3 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 #include "AstroShopping.h"
 #include "Modules/ModuleManager.h"
 
--- Source/AstroShopping/HUD/OverheadWidget.h
+++ Source/AstroShopping/HUD/OverheadWidget.h
@@ -1,14 +1,9 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
 #include "OverheadWidget.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class ASTROSHOPPING_API UOverheadWidget : public UUserWidget
 {
