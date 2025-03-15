86b95c13905e075e05a0f42bc01ddd9189763ad2
Tim <herew26@gmail.com>
2025-03-15 14:13:36 +0300

    ✨ Complete rewrite of `Combinator`

--- Source/AstroShopping/CombinationSystem/Combinator.h
+++ Source/AstroShopping/CombinationSystem/Combinator.h
@@ -6,7 +6,8 @@
 #include "AstroShopping/AI/GenieApiClient.h"
 #include "Combinator.generated.h"
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FCombinationComplete, UStaticMesh*, ResultMesh);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FProductThumbnailLoaded, int32, Index);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FReseted);
 
 UCLASS()
 class ASTROSHOPPING_API ACombinator : public AActor
@@ -21,34 +22,96 @@ public:
 	UFUNCTION(Server, Reliable, BlueprintCallable, Category = "Combination")
 	void Server_StartCombination(const FString& Product1Name, const FString& Product2Name);
 
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnRep_Progress();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnRep_ProductName();
+
+	UFUNCTION(Server, Reliable, BlueprintCallable, Category = "Combination")
+	void Server_RequestLoadProduct(int32 Index);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	FTransform GetProductSpawnPoint() const;
+
 	UPROPERTY(BlueprintAssignable, Category = "Combination")
-	FCombinationComplete OnCombinationComplete;
+	FReseted OnCombinatorReset;
+
+	UPROPERTY(BlueprintAssignable, Category = "Combination")
+	FProductThumbnailLoaded OnProductThumbnailLoaded;
 
 	UPROPERTY(Replicated, BlueprintReadOnly, Category = "Combination")
-	bool bIsCombining = false;
+	bool bIsCombining;
 
-protected:
+	UPROPERTY(BlueprintReadOnly, Category = "Progress")
+	int32 MaxProgress;
+
+	UPROPERTY(ReplicatedUsing = OnRep_Progress, BlueprintReadOnly, Category = "Progress")
+	int32 Progress;
+
+	UPROPERTY(ReplicatedUsing = OnRep_ProductName, BlueprintReadOnly, Category = "Product")
+	FString ProductName;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Product")
+	TArray<UTexture2D*> ProductThumbnails;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Product")
+	bool bIsProductSelected;
 
 private:
-	UPROPERTY(ReplicatedUsing = OnRep_ModelUrls)
-	TArray<FString> ModelUrls;
-
-	UFUNCTION()
-	void OnRep_ModelUrls();
-
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	void Reset();
 
 	UFUNCTION()
 	void OnModelDownloaded(UglTFRuntimeAsset* Asset);
 
 	UFUNCTION()
-	void OnModelLoaded(UStaticMesh* StaticMesh);
+	void OnModelLoaded(UStaticMesh* Mesh);
+
+	void GenerateCombinedItemProps(const FString& InputText, TFunction<void(FString, FString)> OnSuccess, TFunction<void(FString)> OnError);
+
+	void GenerateCombinedItemModels(const FString& Prompt, TFunction<void(FString)> OnError);
+
+	void PollModelStatus(const FString& ModelId, int32 AttemptsRemaining, int32 ModelIndex, TFunction<void(FString)> OnError);
+
+	void IncreaseProgress();
+
+	void ResetProgress();
+
+	void TryLoadProduct();
+
+	void TrySetProductMesh();
+
+	UFUNCTION()
+	void OnRep_ProductThumbnailUrls();
+
+	UFUNCTION()
+	void OnRep_Product();
+
+	UFUNCTION(NetMulticast, Reliable)
+	void Multicast_LoadProduct(int32 Index);
+
+	UPROPERTY(Replicated)
+	TArray<FString> ProductModelUrls;
+
+	UPROPERTY(ReplicatedUsing = OnRep_ProductThumbnailUrls)
+	TArray<FString> ProductThumbnailUrls;
+
+	UPROPERTY(ReplicatedUsing = OnRep_Product)
+	TObjectPtr<class AProduct> Product;
+
+	TObjectPtr<class AProduct> LocalProduct;
+
+	TObjectPtr<UStaticMesh> ProductMesh;
+
+	int32 SelectedProductIndex;
 
 	TUniquePtr<FLlmApiClient> LlmApiClient;
 
 	TUniquePtr<FGenieApiClient> GenieApiClient;
 
-	void GenerateCombinedItemProps(const FString& InputText, TFunction<void(FString, FString)> OnSuccess, TFunction<void(FString)> OnError);
+	int32 MaxPollingAttempts;
 
-	void GenerateCombinedItemModels(const FString& Prompt, TFunction<void(FString)> OnSuccess, TFunction<void(FString)> OnError);
+	float PollDelaySeconds;
 };
