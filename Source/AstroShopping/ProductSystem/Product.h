b6346896f5c04d030a604ab5cfff4a992468c54a
Tim <herew26@gmail.com>
2025-03-15 14:23:55 +0300

    ✨ Add `AProduct` class for product representation with networking support

---
+++ Source/AstroShopping/ProductSystem/Product.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AstroShopping/InteractionSystem/Interactable.h"
+#include "Product.generated.h"
+
+UCLASS()
+class ASTROSHOPPING_API AProduct : public AActor, public IInteractable
+{
+	GENERATED_BODY()
+	
+public:	
+	AProduct();
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly)
+	UStaticMeshComponent* Mesh;
+
+	UPROPERTY(Replicated, VisibleAnywhere, BlueprintReadOnly)
+	FGuid ID;
+
+	UPROPERTY(Replicated, VisibleAnywhere, BlueprintReadOnly, meta = (ExposeOnSpawn = true))
+	FString Name;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (ExposeOnSpawn = true))
+	TSoftObjectPtr<UStaticMesh> ProductMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (ExposeOnSpawn = true))
+	TSoftObjectPtr<UTexture2D> ProductThumbnail;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (ExposeOnSpawn = true))
+	int32 Price;
+
+protected:
+	virtual void BeginPlay() override;
+};
