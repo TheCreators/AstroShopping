553207fa7ea87ce138c6d1e20f8ef9ce6e18a630
Timofey Kochetov <herew26@gmail.com>
2025-03-29 18:24:34 +0300

    ✨ Add product mesh loading and saving

--- Source/AstroShopping/ProductSystem/ProductManager.h
+++ Source/AstroShopping/ProductSystem/ProductManager.h
@@ -3,6 +3,8 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "AstroShopping/SaveSystem/Saveable.h"
+#include "AstroShopping/SaveSystem/Structures/ProductSaveData.h"
+#include "AstroShopping/SaveSystem/Structures/ProductDataSaveData.h"
 #include "ProductManager.generated.h"
 
 UCLASS()
@@ -16,22 +18,72 @@ public:
 	virtual void BeginPlay() override;
 
 	UFUNCTION(BlueprintCallable)
+	bool AddProductData(const FProductDataSaveData& Data, const FGuid& ProductDataID);
+
+	UFUNCTION(BlueprintCallable)
 	class AProduct* SpawnProduct(
 		const FTransform& Transform,
-		const FGuid ID, 
-		const FString& Name,
-		UStaticMesh* ProductMesh,
-		UTexture2D* ProductThumbnail
+		const FGuid ProductDataID
 	);
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
-	TSubclassOf<class AProduct> ProductClass;
+	UFUNCTION(BlueprintCallable)
+	void RequestNewProductSpawn(
+		const FProductDataSaveData& Data, 
+		const FGuid& ProductDataID,
+		const FTransform& Transform
+	);
 
 	void SaveData_Implementation(class UAstroShoppingSaveGame* CurrentGameSave) override;
 
 	void LoadData_Implementation(class UAstroShoppingSaveGame* CurrentGameSave) override;
 
+	UFUNCTION(Server, Reliable)
+	void Server_NotifyOfProductDataMeshLoadCompletion();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<class AProduct> ProductClass;
+
 private:
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UFUNCTION(NetMulticast, Reliable)
+	void Multicast_DownloadProductDataMesh(
+		const FString& MeshURL, 
+		FGuid ProductDataID
+	);
+
+	UFUNCTION()
+	void OnProductDataMeshDownloaded(UglTFRuntimeAsset* Asset);
+
+	UFUNCTION()
+	void OnProductDataMeshLoaded(UStaticMesh* Mesh);
+
+	UFUNCTION()
+	void OnRep_Products();
+
+	UFUNCTION()
+	void LoadNextProductData();
+
+	void LoadProducts();
+
+	TMap<FGuid, FProductDataSaveData> ProductDataToLoad;
+
+	TMap < FGuid, FProductSaveData > ProductsToLoad;
+
+	bool IsLoadingProductDataMesh() const;
+
+	UPROPERTY(ReplicatedUsing = OnRep_Products)
+	TArray<TWeakObjectPtr<class AProduct>> Products;
+
 	UPROPERTY()
-	TMap<FGuid, TWeakObjectPtr<class AProduct>> Products;
+	TMap<FGuid, FProductDataSaveData> ProductData;
+
+	UPROPERTY()
+	TMap<FGuid, UStaticMesh*> ProductDataMeshes;
+
+	int32 NumberOfClientsFinishedLoadingProductDataMesh;
+
+	FGuid CurrentlyLoadingProductDataID;
+
+	bool bHasStartedLoading;
 };
