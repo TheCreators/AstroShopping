5cb2de63055ff0fe8c4d502f0b84a28767d063f4
Timofey Kochetov <herew26@gmail.com>
2025-04-02 20:15:59 +0300

    ✨ Add game mode features and player loading management
    - Implemented constructor for ALobbyGameMode to initialize seamless travel and game level name.
    - Introduced StartGame() to handle server travel based on player count.
    - Added HasLoadingPlayers() to check for loading players.
    - Modified PreLogin() and PostLogin() to manage loading player count.
    - Added LoadingPlayers and GameLevelName variables for enhanced game management.

--- Source/AstroShopping/GameMode/LobbyGameMode.cpp
+++ Source/AstroShopping/GameMode/LobbyGameMode.cpp
@@ -2,16 +2,40 @@
 #include "GameFramework/GameStateBase.h"
 
 
-void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
+ALobbyGameMode::ALobbyGameMode()
 {
-	Super::PostLogin(NewPlayer);
+	bUseSeamlessTravel = true;
+
+	GameLevelName = TEXT("SpaceStation");
+}
+
+void ALobbyGameMode::StartGame()
+{
+	UWorld* World = GetWorld();
+
+	if (!World)
+	{
+		return;
+	}
 
 	int32 NumberOfPlayer = GameState.Get()->PlayerArray.Num();
 
-	if (NumberOfPlayer == 2)
-	{
-		UWorld* World = GetWorld();
-		if (!ensure(World != nullptr)) return;
-		World->ServerTravel("/Game/AstroShopping/Maps/Game?listen");
-	}
+	World->ServerTravel(TEXT("/Game/AstroShopping/Maps/" + GameLevelName + "?listen?players=" + FString::FromInt(NumberOfPlayer)));
+}
+
+bool ALobbyGameMode::HasLoadingPlayers() const
+{
+	return LoadingPlayers > 0;
+}
+
+void ALobbyGameMode::PreLogin(const FString& Options, const FString& Address, const FUniqueNetIdRepl& UniqueId, FString& ErrorMessage)
+{
+	Super::PreLogin(Options, Address, UniqueId, ErrorMessage);
+	LoadingPlayers++;
+}
+
+void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+	LoadingPlayers--;
 }
--- Source/AstroShopping/GameMode/LobbyGameMode.h
+++ Source/AstroShopping/GameMode/LobbyGameMode.h
@@ -10,5 +10,21 @@ class ASTROSHOPPING_API ALobbyGameMode : public AGameModeBase
 	GENERATED_BODY()
 	
 public:
+	ALobbyGameMode();
+
+	UFUNCTION(BlueprintCallable)
+	void StartGame();
+
+	UFUNCTION(BlueprintPure)
+	bool HasLoadingPlayers() const;
+
+	UPROPERTY(EditDefaultsOnly)
+	FString GameLevelName;
+
+private:
+	virtual void PreLogin(const FString& Options, const FString& Address, const FUniqueNetIdRepl& UniqueId, FString& ErrorMessage) override;
+
 	virtual void PostLogin(APlayerController* NewPlayer) override;
+
+	int32 LoadingPlayers = 0;
 };
